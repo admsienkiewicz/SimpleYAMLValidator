1c833aaffc766c7e49ecdfaac913a4d29193b913
admsienkiewicz <62251718+admsienkiewicz@users.noreply.github.com>
2021-04-15 18:08:50 +0200

    validator

---
+++ validator.py
@@ -0,0 +1,44 @@
+from scanner import *
+from pars import *
+
+input_string = '''
+version: "3.9"
+
+services:
+  wordpress:
+    image: wordpress
+    ports:
+      - "8080:80"
+    networks:
+      - overlay
+    deploy:
+      mode: replicated
+      replicas: 2
+      endpoint_mode: vip
+
+  mysql:
+    image: mysql
+    volumes:
+      - db-data:/var/lib/mysql/data
+    networks:
+      - overlay
+    deploy:
+      mode: replicated
+      replicas: 2
+      endpoint_mode: dnsrr
+
+volumes:
+  db-data:
+
+networks:
+  overlay:
+
+'''
+
+print(input_string)
+scanner = Scanner(input_string)
+#print(scanner.tokens)
+
+parser = Parser(scanner)
+parser.start()
+  
